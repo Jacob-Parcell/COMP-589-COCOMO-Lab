444522de39430d917bd1a577256a70d987a9cbcc
Jacob-Parcell <jakeparcell14@gmail.com>
2026-05-07 19:53:07 -0700

    shortened palindrome logic

--- Palindrom.py
+++ Palindrom.py
@@ -1,18 +1,12 @@
-s = input("Enter a palindrome: ") 
-i, j = 0, len(s) - 1  
-is_palindrome = True  
+def playGame():
+    s = input("Enter a palindrome: ") 
+    i, j = 0, len(s) - 1  
+    is_palindrome = True  
 
-while i < j:
-    if s[i] != s[j]:  
-        is_palindrome = False
-        break
-    i += 1
-    j -= 1
-
-if is_palindrome:
-    print("Yes") 
-else:
-    print("No")
+    if s == s[::-1]:
+        print("Yes")
+    else:
+        print("No")
 
 
 def printMenu():
@@ -23,7 +17,6 @@ def printMenu():
 
 def handleUserInput(userInput):
     match userInput:
-
         case 1:
             playGame()
         case 2:
